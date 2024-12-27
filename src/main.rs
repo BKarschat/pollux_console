fad7491643b1348a23c2f942d7b8bd263a458359
Bastian Karschat <bastian.karschat@gmail.com>
2024-12-27 15:07:37 +0100

    Add libs tracing an color-eyre for tracing errors nicely. Console App should be based on raratui

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
-fn main() {
-    println!("Hello, world!");
-    let foo: i32 = 6;
-    println!("{}", foo);
+use color_eyre::Result;
+use crossterm::event::{self, Event};
+use ratatui::{DefaultTerminal, Frame};
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
+fn main() -> Result<()> {
+    // a builder for 'FmtSubscriber'
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting the default to the subscriber failed!");
+
+    color_eyre::install()?;
+    info!("Hello, first console app in rust!");
+    let main_terminal = ratatui::init();
+    let result_main = run(main_terminal);
+    ratatui::restore();
+    Ok(())
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if matches!(event::read()?, Event::Key(_)) {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("Rust App", frame.area());
 }
