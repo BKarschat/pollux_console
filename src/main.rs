b1bff189fa664a43c0b9d0f5cecd9d8ae6c00a40
Bastian Karschat <bastian.karschat@gmail.com>
2025-04-11 13:37:12 +0200

    Add header and footer to app. Remove Demo Code

--- src/main.rs
+++ src/main.rs
@@ -1,198 +1,72 @@
-use color_eyre::{eyre::Ok, owo_colors::OwoColorize, Result};
-use std::io;
-use tracing::{event, info, Level};
-use tracing_subscriber::FmtSubscriber;
-
-use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind};
-use ratatui::{
-    buffer::Buffer,
-    layout::Rect,
-    style::Stylize,
-    symbols::border,
-    text::{Line, Text},
-    widgets::{Block, Paragraph, Widget},
-    DefaultTerminal, Frame,
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{
+        self, disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+    },
 };
+use ratatui::{
+    backend::CrosstermBackend,
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::Span,
+    widgets::{Block, Borders, Paragraph},
+    Terminal,
+};
+use std::io;
+use std::time::Duration;
 
-#[derive(Debug, Default)]
-pub struct App {
-    counter: u8,
-    exit: bool,
-}
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
 
-impl App {
-    /// runs the applications main loop
+    // Loop
+    let result = run_app(&mut terminal);
 
-    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<()> {
-        while !self.exit {
-            terminal.draw(|frame| self.draw(frame))?;
-            self.handle_events()?;
-        }
-        Ok(())
-    }
-
-    fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
-    }
-
-    fn handle_events(&mut self) -> Result<()> {
-        match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
-            }
-            _ => {}
-        };
-        Ok(())
-    }
-
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Char('q') => self.exit(),
-            KeyCode::Left => self.decrement_counter(),
-            KeyCode::Right => self.increment_counter(),
-            _ => {}
-        }
-    }
-
-    fn exit(&mut self) {
-        self.exit = true;
-    }
-
-<<<<<<< HEAD
-    fn decrement_counter(&mut self) {
-        // for error handling purpose use saturating_sub instead!
-        self.counter += 1;
-    }
-
-    fn increment_counter(&mut self) {
-        // for error handling purpose use saturating_sub instead
-=======
-    fn increment_counter(&mut self) {
-        self.counter += 1;
-    }
-
-    fn decrement_counter(&mut self) {
->>>>>>> bd3d4bb6f7e9aee29990e8c7e7ab4ec8230ddc50
-        self.counter -= 1;
-    }
-}
-
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Line::from(" Pollux gonna sniff ".bold());
-        let instructions = Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]);
-
-        let block = Block::bordered()
-            .title(title.centered())
-            .title_bottom(instructions.centered())
-            .border_set(border::THICK);
-
-        let counter_text = Text::from(vec![Line::from(vec![
-            "Value: ".into(),
-            self.counter.to_string().yellow(),
-        ])]);
-
-        Paragraph::new(counter_text)
-            .centered()
-            .block(block)
-            .render(area, buf);
-    }
-}
-fn main() -> Result<()> {
-    // a builder for 'FmtSubscriber'
-    let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::TRACE)
-        .finish();
-    tracing::subscriber::set_global_default(subscriber)
-        .expect("setting the default to the subscriber failed!");
-
-    color_eyre::install()?;
-    info!("Hello, first console app in rust!");
-    let main_terminal = ratatui::init();
-    let result_main = run(main_terminal);
+    // CLeanup
+    //
     ratatui::restore();
-    result_main
+
+    result
 }
 
-fn run(mut terminal: DefaultTerminal) -> Result<()> {
+fn run_app(terminal: &mut ratatui::DefaultTerminal) -> io::Result<()> {
     loop {
-        terminal.draw(render)?;
-        if matches!(crossterm::event::read()?, Event::Key(_)) {
-            break Ok(());
+        terminal.draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints([
+                    Constraint::Length(3),
+                    Constraint::Min(0),
+                    Constraint::Length(1),
+                ])
+                .split(f.size());
+
+            let header = Paragraph::new("=== Header ===")
+                .style(Style::default().fg(Color::Yellow))
+                .block(Block::default().borders(Borders::ALL).title("Header"));
+
+            let body = Paragraph::new("Das ist der Body")
+                .block(Block::default().borders(Borders::ALL).title("Body"));
+
+            let footer = Paragraph::new(Span::styled(
+                "Press q to quit",
+                Style::default().add_modifier(Modifier::ITALIC),
+            ));
+
+            f.render_widget(header, chunks[0]);
+            f.render_widget(body, chunks[1]);
+            f.render_widget(footer, chunks[2]);
+        })?;
+
+        //Event handling
+
+        if event::poll(Duration::from_millis(200))? {
+            if let Event::Key(key) = event::read()? {
+                if key.code == KeyCode::Char('q') {
+                    return Ok(());
+                }
+            }
         }
-    }
-}
-
-fn render(frame: &mut Frame) {
-    frame.render_widget("Rust App", frame.area());
-}
-
-#[cfg(test)]
-mod tests {
-<<<<<<< HEAD
-    use crate::App;
-
-    #[test]
-    fn handle_key_event() -> std::io::Result<()> {
-        let mut app = App::default();
-        app.handle_key_event(crossterm::event::KeyCode::Right.into());
-        assert_eq!(app.counter, 1);
-
-        app.handle_key_event(crossterm::event::KeyCode::Left.into());
-        assert_eq!(app.counter, 0);
-
-        let mut app = App::default();
-        app.handle_key_event(crossterm::event::KeyCode::Char('q').into());
-=======
-    use super::*;
-    use ratatui::style::Style;
-
-    #[test]
-    fn render() {
-        let app = App::default();
-        let mut buf = Buffer::empty(Rect::new(0, 0, 50, 4));
-
-        app.render(buf.area, &mut buf);
-
-        let mut expected = Buffer::with_lines(vec![
-            "┏━━━━━━━━━━━━━ Pollux gonna sniff ━━━━━━━━━━━━━━━┓",
-            "┃                    Value: 0                    ┃",
-            "┃                                                ┃",
-            "┗━ Decrement <Left> Increment <Right> Quit <Q> ━━┛",
-        ]);
-        let title_style = Style::new().bold();
-        let counter_style = Style::new().yellow();
-        let key_style = Style::new().blue().bold();
-        expected.set_style(Rect::new(14, 0, 22, 1), title_style);
-        expected.set_style(Rect::new(28, 1, 1, 1), counter_style);
-        expected.set_style(Rect::new(13, 3, 6, 1), key_style);
-        expected.set_style(Rect::new(30, 3, 7, 1), key_style);
-        expected.set_style(Rect::new(43, 3, 4, 1), key_style);
-
-        assert_eq!(buf, buf);
-    }
-
-    #[test]
-    fn handle_key_event() -> io::Result<()> {
-        let mut app = App::default();
-        app.handle_key_event(KeyCode::Right.into());
-        assert_eq!(app.counter, 1);
-
-        app.handle_key_event(KeyCode::Left.into());
-        assert_eq!(app.counter, 0);
-
-        let mut app = App::default();
-        app.handle_key_event(KeyCode::Char('q').into());
->>>>>>> bd3d4bb6f7e9aee29990e8c7e7ab4ec8230ddc50
-        assert!(app.exit);
-
-        Ok(())
     }
 }
