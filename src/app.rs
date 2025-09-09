aca671eecf66ba436fc3ede64681d3bfb98a6337
Bastian Karschat <bastian.karschat@protonmail.com>
2025-09-09 14:44:34 +0200

    rewrite Eventhandler

--- src/app.rs
+++ src/app.rs
@@ -4,26 +4,33 @@ use std::{
     time::{Duration, Instant},
 };
 
-use crossterm::event;
+use crossterm::event::{self, KeyCode};
 use ratatui::{self, DefaultTerminal};
+use tokio;
+use tokio_stream::StreamExt;
 
-use crate::{modules::network, ui};
+use crate::{modules::network, ui::*};
 
+#[derive(Debug)]
 pub struct App {
     pub running: bool,
     pub logs: Vec<String>,
-    pub scroll: u16,
     ui_handler: ui::UiHandler,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            running: true,
+            logs: Vec::new(),
+            ui_handler: ui::UiHandler::new(),
+        }
+    }
 }
 
 impl App {
     pub fn new() -> Self {
-        Self {
-            running: true,
-            logs: Vec::new(),
-            scroll: 0,
-            ui_handler: ui::UiHandler::new(),
-        }
+        Self::default()
     }
 
     fn render_app(&mut self, tui: &mut DefaultTerminal) -> Result<()> {
@@ -31,57 +38,35 @@ impl App {
         Ok(())
     }
 
-    fn handle_events(&mut self) -> Result<()> {
-        let frame_rate = Duration::from_secs_f64(1.0 / 60.0);
-        if !event::poll(frame_rate)? {
+    fn handle_event(&mut self, event: &Event) -> Result<()> {
+        if let Some(key) = event.as_key_press_event() {
+            match key.code {
+                KeyCode::Char('q') | KeyCode::Esc => self.running = false,
+                KeyCode::Char('j') | KeyCode::Down => self.pull_r,
+
+                _ => Ok(()),
+            }
             return Ok(());
         }
         if event::read()?.is_key_press() {
             self.running = false;
+        }
         Ok(())
     }
-    
-    pub fn run_app(&mut self, terminal: &mut DefaultTerminal) -> Result<()> {
-        // 
+
+    pub async fn run(mut self, terminal: &mut DefaultTerminal) -> Result<()> {
+        let frame_rate = Duration::from_secs_f64(1.0 / 60.0);
+        let mut interval = tokio::time::interval(frame_rate);
+        let mut events = EventStream::new();
+
         while self.running {
-            self.render_app(terminal)?;
-            self.handle_events()?;
-        }
-        Ok(())
-    }
-
-
-        // Kill threa d if user quits the add, so you need a tic
-        let tick_rate = Duration::from_secs_f64(1.0 / 60.0);
-        let mut last_tick = Instant::now();
-
-        //init ratatui terminal
-
-        let terminal = ratatui::init();
-
-        //init channels
-        let (tx, rx) = mpsc::channel();
-        let tx_clone = tx.clone();
-
-        // init modules
-        let network_interfaces = network::InterfaceData::new(tx_clone);
-
-        // add network interfaces to the ui
-        let ui_handler = ui::ui::UiHandler::new();
-
-        ui_handler.add_widget(Box::new(ui::network::InterfaceListWidget::new()));
-        // Todo add data to UI!
-        loop {
-            if !self.running {
-                break;
+            tokio::select! {
+                _ = interval.tick() => {terminal.draw(|frame| self.render(frame))?;},
+                Some(Ok(event)) = events.next() => self.handle_event(&event),
             }
-            if let Ok(interfaces) = rx.recv() {
-                // TODO what is happing here?!
-                terminal.draw(|f| ui_handler::render_ui(f, &));
-            }
-            input::handle_input(self);
+            //self.render_app(terminal)?;
+            //self.handle_events()?;
         }
-        ratatui::restore();
         Ok(())
     }
 }
