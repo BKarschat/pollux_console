a44460c13cd3186624d16190d6a401976ae7cfb2
Bastian Karschat <bastian.karschat@protonmail.com>
2025-09-09 15:15:35 +0200

    sync

--- src/event.rs
+++ src/event.rs
@@ -1,18 +1,1 @@
-use crate::app::App;
-use crossterm::event::{self, Event, KeyCode};
-use std::time::Duration;
-
-pub fn handle_input(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
-    if event::poll(Duration::from_millis(100))? {
-        if let Event::Key(key) = event::read() {
-            match key.code {
-                KeyCode::Char('q') => app.running = false,
-                KeyCode::Down => app.scroll += 1,
-                KeyCode::Up => app.scroll += 1,
-                //TODO Enter
-                _ => {}
-            }
-        }
-    }
-    OK(())
-}
+use color_eyre::eyre
